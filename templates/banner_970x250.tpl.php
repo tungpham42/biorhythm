ee3c5b7620ed7545a3773034c78dd80d7ef09b79
Tung Pham <tung.42@gmail.com>
2014-12-22 08:41:41 +0700

    Major changes
    2014-12-22

---
+++ templates/banner_970x250.tpl.php
@@ -0,0 +1,6 @@
+<div class="billboard_banner w970">
+<ins class="adsbygoogle" data-ad-client="ca-pub-3585118770961536" data-ad-slot="8320218482"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
+</div>
