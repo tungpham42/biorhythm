9facca1e95a5c9dbe453667c3f3bb2a6f6e4f19e
Tung Pham <tung.42@gmail.com>
2014-12-27 16:44:07 +0700

    Major fixes
    2014-12-27

--- templates/head.tpl.php
+++ templates/head.tpl.php
@@ -3,14 +3,14 @@
 <meta name="robots" content="index, follow">
 <meta name="viewport" content="width=device-width, height=device-height, user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0">
 <meta name="description" content="<?php echo $head_description; ?>">
-<meta name="keywords" content="nhip sinh hoc, nhipsinhhoc, nhịp sinh học, chu ky sinh hoc cua nguoi, nhip sinh hoc co the nguoi, nhịp sinh học là gì, nhip sinh hoc hom nay, nhip sinh hoc voi suc khoe, nhip sinh hoc cua ban, nhip sinh hoc cua con nguoi, nhip sinh hoc hang ngay, nhip sinh hoc cua co the la gi, nhip sinh hoc 12 cung hoang dao, biorhythm, биоритм, biorritmo, 生物节律, バイオリズム, xem biểu đồ sinh học, xem ngay tot xau, dong ho sinh hoc, phan mem nhip sinh hoc, xem tu vi, cach tinh nhip sinh hoc, sinh hoc, boi nhip sinh hoc, nhip sinh hoc theo thang, bieu do nhip sinh hoc, bói nhịp sinh học, biểu đồ nhịp sinh học, bieu do sinh hoc, xem ngay sinh, xem ngay, xem ngày sinh, xem ngày, tu vi, tử vi<?php echo (isset($_GET['fullname']) && $_GET['fullname'] != '') ? ', '.$_GET['fullname']: ''; ?>, bmi, tính chỉ số bmi, tinh chi so bmi">
+<meta name="keywords" content="<?php echo ($keywords != '') ? $keywords.', ': ''; ?>nhip sinh hoc, nhipsinhhoc, nhịp sinh học, chu ky sinh hoc cua nguoi, nhip sinh hoc co the nguoi, nhịp sinh học là gì, nhip sinh hoc hom nay, nhip sinh hoc voi suc khoe, nhip sinh hoc cua ban, nhip sinh hoc cua con nguoi, nhip sinh hoc hang ngay, nhip sinh hoc cua co the la gi, nhip sinh hoc 12 cung hoang dao, biorhythm, биоритм, biorritmo, 生物节律, バイオリズム, xem biểu đồ sinh học, xem ngay tot xau, dong ho sinh hoc, phan mem nhip sinh hoc, xem tu vi, cach tinh nhip sinh hoc, sinh hoc, boi nhip sinh hoc, nhip sinh hoc theo thang, bieu do nhip sinh hoc, bói nhịp sinh học, biểu đồ nhịp sinh học, bieu do sinh hoc, xem ngay sinh, xem ngay, xem ngày sinh, xem ngày, tu vi, tử vi<?php echo (isset($_GET['fullname']) && $_GET['fullname'] != '') ? ', '.$_GET['fullname']: ''; ?>, bmi, tính chỉ số bmi, tinh chi so bmi">
 <meta name="author" content="Tung Pham">
 <meta property="og:type" content="website">
 <meta property="og:url" content="<?php echo str_replace('&', '&amp;', current_url()); ?>">
 <meta property="og:title" content="<?php echo $head_title; ?>">
 <meta property="og:description" content="<?php echo $head_description; ?>">
 <meta property="og:image" content="<?php echo 'http://nhipsinhhoc.vn/img/img_desc_'.((has_dob() && is_birthday()) ? 'birthday': 'logo').'.png'; ?>">
-<link rel="stylesheet" href="/min/b=css&amp;f=fonts.css,normalize.css,install-button.css,jquery.listnav.css,jquery.contextMenu.css,bootstrap.css,m-styles.css,default.css,ui-blue/jquery-ui.css,nprogress.css&amp;368" />
+<link rel="stylesheet" href="/min/b=css&amp;f=fonts.css,normalize.css,install-button.css,jquery.listnav.css,jquery.contextMenu.css,bootstrap.css,m-styles.css,default.css,ui-blue/jquery-ui.css,nprogress.css&amp;372" />
 <link rel="stylesheet" href="/min/f=css/print.css" media="print" />
 <link rel="icon" href="/favicon.ico" />
 <link rel="apple-touch-icon" href="/app-icons/apple-touch-icon-iphone.png">
---
+++ xemngay/index.php
@@ -0,0 +1,5 @@
+<?php
+$_GET['lang'] = 'vi';
+$_GET['p'] = 'lunar';
+require_once realpath($_SERVER['DOCUMENT_ROOT']).'/index.php';
+?>
