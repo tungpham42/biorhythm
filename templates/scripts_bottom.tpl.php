a763ece921d707984c8da59efed47291b58340dd
Tung Pham <tung.42@gmail.com>
2015-01-29 16:16:24 +0700

    to top
    2015-01-29

--- templates/scripts_bottom.tpl.php
+++ templates/scripts_bottom.tpl.php
@@ -28,6 +28,23 @@ endif;
 $(document).ready(function(){
 	manipulateLang();
 	manipulateButtons();
+	var offset = 300, offset_opacity = 1200, scroll_top_duration = 700, to_top = $('.to-top');
+
+	//hide or show the "back to top" link
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? to_top.addClass('is-visible') : to_top.removeClass('is-visible fade-out');
+		if( $(this).scrollTop() > offset_opacity ) {
+			to_top.addClass('fade-out');
+		}
+	});
+	//smooth scroll to top
+	to_top.on('click', function(e){
+		e.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
 });
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
