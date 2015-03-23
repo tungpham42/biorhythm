5f82f104aad7f1f2b391379a112fbd008657c46d
Tung Pham <tung.42@gmail.com>
2015-03-23 11:09:13 +0700

    Minor change
    2015-03-23

--- templates/admin/user/index.tpl.php
+++ templates/admin/user/index.tpl.php
@@ -1,5 +1,19 @@
+<input id="user_search" type="text" name="user_search" size="60" maxlength="128" />
 <div id="admin_user">
 <?php
 echo list_users();
 ?>
 </div>
+<script>
+$("#user_search").on({
+	keyup: function(){
+		$("#admin_user").load("/triggers/admin_user.php",{page:1,keyword:$("#user_search").val()});
+	},
+	keydown: function(){
+		$("#admin_user").load("/triggers/admin_user.php",{page:1,keyword:$("#user_search").val()});
+	},
+	change: function(){
+		$("#admin_user").load("/triggers/admin_user.php",{page:1,keyword:$("#user_search").val()});
+	}
+});
+</script>
